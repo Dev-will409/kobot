fc79e4838ac2e0752acb5c4ee29c05ecc8d5c3ca
DESKTOP-UBT5LFA\rlasl <rlaslstjr@gmail.com>
2022-10-26 17:13:36 +0900

    .

--- src/pages/logIn/index.js
+++ src/pages/logIn/index.js
@@ -1,36 +1,53 @@
 import React, {useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Card, CardBody, CardSubtitle, CardText, CardTitle, Input} from "reactstrap";
+import {Link} from "react-router-dom";
+import styled from 'styled-components';
+
+const LoginTemplate = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 
 const logIn = () => {
     return(
-        <div><Card
-            body
-            style={{
-                width:'30rem',
-                display : 'flex',
-                textAlign:'center'
-            }}
-        >
-            <CardBody>
-                <CardTitle tag="h1">
-                    Kobot
-                </CardTitle>
-                <CardSubtitle
-                    tag="h6"
+        <div>
+            <LoginTemplate>
+                <Card
+                    body
+                    style={{
+                        width: "360px",
+                        height: "300px",
+                        padding: "20px",
+                    }}
                 >
-                    Login
-                </CardSubtitle>
-                <CardText>
-
-                    ID : <Input/>
-                    Password : <Input/>
-                </CardText>
-                <Button>
-                    login
-                </Button>
-            </CardBody>
-        </Card>
+                    <CardBody>
+                        <CardTitle tag="h1">
+                            Kobot
+                        </CardTitle>
+                        <CardSubtitle
+                            tag="h6"
+                        >
+                            Login
+                        </CardSubtitle>
+                        <CardText>
+                            <Input placeholder={"ID"}/>
+                            <Input placeholder={"PASSWORD"}/>
+                        </CardText>
+                        <Button>
+                            <Link to={'/trading'}>login</Link>
+                        </Button>
+                    </CardBody>
+                </Card>
+            </LoginTemplate>
         </div>
     )
 };
