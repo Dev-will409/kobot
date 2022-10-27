557ffaed53871dbe7f6b9fee9adcffe333fdc329
Kim minseok <>
2022-10-27 18:56:54 +0900

    .

--- src/pages/logIn/index.js
+++ src/pages/logIn/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Card, CardBody, CardSubtitle, CardText, CardTitle, Input} from "reactstrap";
+import {Button, Card, CardBody, CardSubtitle, CardText, CardTitle, Col, Input, Row} from "reactstrap";
 import {Link} from "react-router-dom";
 import styled from 'styled-components';
 
@@ -21,32 +21,31 @@ const logIn = () => {
     return(
         <div>
             <LoginTemplate>
-                <Card
-                    body
-                    style={{
-                        width: "360px",
-                        height: "300px",
-                        padding: "20px",
-                    }}
-                >
-                    <CardBody>
-                        <CardTitle tag="h1">
-                            Kobot
-                        </CardTitle>
-                        <CardSubtitle
-                            tag="h6"
+                <Row>
+                    <Col>
+                        <Card
+                            body
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                padding: "20px",
+                            }}
                         >
-                            Login
-                        </CardSubtitle>
-                        <CardText>
-                            <Input placeholder={"ID"}/>
-                            <Input placeholder={"PASSWORD"}/>
-                        </CardText>
-                        <Button>
-                            <Link to={'/trading'}>login</Link>
-                        </Button>
-                    </CardBody>
-                </Card>
+                            <CardBody>
+                                <CardTitle tag="h1" style={{textAlign:'center', margin: "30px"}}>
+                                    Kobot
+                                </CardTitle>
+                                <CardText style={{margin:"10px"}}>
+                                    <Input style={{margin:"10px"}} placeholder={"ID"}/>
+                                    <Input style={{margin:"10px"}} placeholder={"PASSWORD"}/>
+                                </CardText>
+                                <Button>
+                                    <Link to={'/trading'}>login</Link>
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
             </LoginTemplate>
         </div>
     )
--- src/pages/trading/index.js
+++ src/pages/trading/index.js
@@ -1,9 +1,87 @@
 import React, {useEffect, useState} from "react";
+import {Button, Card, Col, Row, Table} from "reactstrap";
 
 const trading = () => {
     return(
         <div>
-            trading
+            <Row>
+                <Col md={2}>
+                    <Row>
+                        <Card>
+
+                        </Card>
+                    </Row>
+                    <Row>
+                        <Card>
+                            <Table>
+                                <tbody>
+                                    <tr>
+                                        <th>잔고</th>
+                                        <td>1000000</td>
+                                    </tr>
+                                    <tr>
+                                        <th>수익</th>
+                                        <td>1000000</td>
+                                    </tr>
+                                </tbody>
+                                <Button>API</Button>
+                            </Table>
+                        </Card>
+                    </Row>
+                </Col>
+                <Col md={7}>
+                    <Card>
+                        dsfds
+                    </Card>
+                </Col>
+                <Col md={3}>
+                    <Card>
+
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={2}>
+                    <Card>
+                        <Table>
+                            <thead>
+                                <th>항목</th>
+                                <th>설정값</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>파라미터1</td>
+                                    <td>########</td>
+                                </tr>
+                                <tr>
+                                    <td>파라미터2</td>
+                                    <td>########</td>
+                                </tr>
+                                <tr>
+                                    <td>파라미터3</td>
+                                    <td>########</td>
+                                </tr>
+                                <tr>
+                                    <td>파라미터4</td>
+                                    <td>########</td>
+                                </tr>
+                            </tbody>
+                            <Button>API</Button>
+                        </Table>
+                    </Card>
+                </Col>
+                <Col md={7}>
+                    <Card>
+
+                    </Card>
+                </Col>
+                <Col md={3}>
+                    <Card>
+
+                    </Card>
+                </Col>
+            </Row>
+
         </div>
     )
 };
